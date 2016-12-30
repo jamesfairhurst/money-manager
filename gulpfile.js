261e3d2e7ff5b7a479869b75ecf2b9677db6b31e
James Fairhurst <info@jamesfairhurst.co.uk>
2016-12-30 20:09:02 +0000

    Add Font Awesome

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
     mix.sass('app.scss')
        // .webpack('app.js')
        ;
