e06926a5b8fb83be1c0f336d86a8d4565b5cd39c
James Fairhurst <info@jamesfairhurst.co.uk>
2017-01-02 21:09:32 +0000

    Add dashboard monthly & weekly stats & graphs

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,42 @@
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    // @todo urgh refactor this using collections
+    $tagsThisMonth = [];
+    $tagsThisWeek = [];
+
+    foreach (App\Transaction::with('tags')->month()->get() as $transaction) {
+        foreach ($transaction->tags as $tag) {
+            if (! isset($tagsThisMonth[$tag->id])) {
+                $tagsThisMonth[$tag->id] = [
+                    'name' => $tag->name,
+                    'amount' => 0
+                ];
+
+                $tagsThisMonth[$tag->id]['amount'] += $transaction->amount;
+            }
+        }
+    }
+
+    foreach (App\Transaction::with('tags')->week()->get() as $transaction) {
+        foreach ($transaction->tags as $tag) {
+            if (! isset($tagsThisWeek[$tag->id])) {
+                $tagsThisWeek[$tag->id] = [
+                    'name' => $tag->name,
+                    'amount' => 0
+                ];
+
+                $tagsThisWeek[$tag->id]['amount'] += $transaction->amount;
+            }
+        }
+    }
+
+    /*dd(App\Transaction::with('tags')->week()->get()->transform(function ($item, $key) {
+        return array_combine($item->tags->pluck('name')->toArray(), array_fill(0, $item->tags->count(), $item->amount));
+        // return ['amount' => $item->amount, 'tags' => $item->tags->pluck('name')->toArray()];
+    }));*/
+
+    return view('dashboard', ['tagsThisMonth' => $tagsThisMonth, 'tagsThisWeek' => $tagsThisWeek]);
 });
 
 Route::resource('transactions', 'TransactionController');
