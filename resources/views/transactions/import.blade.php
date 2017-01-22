0336aa7ddacdd712690790aafa0877315ecd298f
James Fairhurst <info@jamesfairhurst.co.uk>
2017-01-22 20:11:38 +0000

    Central align import checkboxes

--- resources/views/transactions/import.blade.php
+++ resources/views/transactions/import.blade.php
@@ -77,7 +77,7 @@
                         <td>{{ $cell }}</td>
                         @endforeach
                         <td><input type="name" name="tags[{{ $key+1 }}]" class="form-control"></td>
-                        <td><input type="checkbox" name="rows[]" value="{{ $key+1 }}"></td>
+                        <td class="text-center"><input type="checkbox" name="rows[]" value="{{ $key+1 }}"></td>
                     </tr>
                     @endforeach
                 </tbody>
